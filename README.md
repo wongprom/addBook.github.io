189c69bf161e0e33eb8b54dc7ce91c67d11276a2
wongprom <jimmybackstrom@hotmail.com>
2018-09-05 22:45:13 +0200

    Convert ES5 to ES6

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Bult With
 
 - Vanilla JavaScript, Object Oriented Programming.
-- ES5 **AND** ES6 Syntax
+- ES5 (Using prototype) **AND** ES6 (Using class) Syntax
 - Css [Skeleton](https://cdnjs.com/libraries/skeleton)
---
+++ appEs6.js
@@ -0,0 +1,95 @@
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+class UI {
+  addBookTolist(book) {
+    const list = document.querySelector('#book-list');
+    //Create tr element
+    const row = document.createElement('tr');
+    //Insert columns
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="x" class="delete">X</a></td>
+    `;
+    list.appendChild(row);
+  }
+
+  showAlert(message, className) {
+    //Creatediv element
+    const div = document.createElement('div');
+    //Add classes
+    div.className = `alert ${className}`;
+    //Add text
+    div.appendChild(document.createTextNode(message));
+    //Get parent
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    //Insert alert
+    container.insertBefore(div, form);
+    //SetTimeout for alert
+    setTimeout(function() {
+      document.querySelector('.alert').remove();
+    }, 3000);
+  }
+
+  deleteBook(target) {
+    if (target.className === 'delete') {
+      target.parentElement.parentElement.remove();
+    }
+  }
+
+  clearFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#isbn').value = '';
+  }
+}
+
+//Event listner for add book
+document.querySelector('#book-form').addEventListener('submit', function(e) {
+  //Get form values
+  (title = document.querySelector('#title').value),
+    (author = document.querySelector('#author').value),
+    (isbn = document.querySelector('#isbn').value);
+  //Instantiat book
+  const book = new Book(title, author, isbn);
+
+  //Instantiat UI
+  const ui = new UI();
+
+  //Validate
+  if (title === '' || author === '' || isbn === '') {
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
+    //Add book ti list
+    ui.addBookTolist(book);
+
+    //Show success
+    ui.showAlert('Book added', 'success');
+    //Clear fields
+    ui.clearFields();
+  }
+
+  //Prevent window to load
+  e.preventDefault();
+});
+
+//Event listner for delete
+document.querySelector('#book-list').addEventListener('click', function(e) {
+  //Instantiat UI
+  const ui = new UI();
+
+  //Delete book
+  ui.deleteBook(e.target);
+
+  //Show message
+  ui.showAlert('Deleted book', 'success');
+  e.preventDefault();
+});
